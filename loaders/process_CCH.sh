93a287d06963f5c23083356b945d9bbd2d9a8323
jalexander <jason_alexander@berkeley.edu>
2018-05-14 14:32:31 -0700

    initial upload of first version of cumulative cch update shell script, written by Jason Alexander

---
+++ loaders/process_CCH.sh
@@ -0,0 +1,24 @@
+#this parses the data file for a participant, then it sorts the error log file.  
+#this used to be done manually with programs such as vi or vim
+#this has an advantage because it preserves the order of the error log (you can see all the errors a specimen record produces)
+#and it creates a unique sorted error log file
+
+echo "$?                                          parsing BLMAR";
+perl /JEPS-master/CCH/Loaders/BLMAR/parse_BLMAR.pl
+echo "$?                                          sorting log file";
+cp log.txt /JEPS-master/CCH/Loaders/BLMAR/
+sort -u log.txt > /JEPS-master/CCH/Loaders/BLMAR/BLMAR_log_sort.txt
+
+echo "$?                                          NEXT";
+
+echo "$?                                          parsing OBI";
+perl /JEPS-master/CCH/Loaders/OBI/parse_OBI.pl
+echo "$?                                          sorting log file";
+cp /JEPS-master/CCH/Loaders/OBI/OBI_out.txt /JEPS-master/CCH/bulkload/input/new_files/
+cp log.txt /JEPS-master/CCH/Loaders/OBI/
+sort -u log.txt > /JEPS-master/CCH/Loaders/OBI/OBI_log_sort.txt
+
+echo "$?                                          NEXT";
+
+
+echo "$?                                          process complete";
